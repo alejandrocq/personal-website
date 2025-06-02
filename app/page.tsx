859bdb59a2d43375b55132f49f2a2d1cfb08033f
Alejandro Castilla Quesada <alejandrocastilla@geotab.com>
2025-06-03 00:21:39 +0200

    Create constants.ts and move "aboutMe" text there

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import {FaLinkedin, FaGithub, FaEnvelope} from "react-icons/fa";
+import {aboutMe} from "@/app/utils/constants";
 
 export default function Home() {
     return (
@@ -23,7 +24,7 @@ export default function Home() {
             </div>
 
             <p className="text-lg text-gray-100 text-justify">
-                I am Alejandro, an experienced Software Developer with a proven track record of delivering impactful solutions within the fleet management industry. As a Senior Software Developer at Geotab, I leverage my expertise in Java, C#, and TypeScript to maintain and enhance a leading-edge compliance platform, originally developed at Intendia. My focus includes real-time data ingestion, algorithm optimization, ensuring adherence to evolving regulatory requirements, and developing performant and user-friendly interfaces. I am deeply committed to continuous improvement, both in the code I write and the practices of the team around me, and to a culture of mentorship and knowledge sharing.
+                {aboutMe}
             </p>
         </section>
     );
---
+++ app/utils/constants.ts
@@ -0,0 +1,8 @@
+export const aboutMe = `
+I am Alejandro, an experienced Software Developer with a proven track record of delivering impactful solutions within the
+fleet management industry. As a Senior Software Developer at Geotab, I leverage my expertise in Java, C#, and TypeScript
+to maintain and enhance a leading-edge compliance platform, originally developed at Intendia. My focus includes real-time data
+ingestion, algorithm optimization, ensuring adherence to evolving regulatory requirements, and developing performant and
+user-friendly interfaces. I am deeply committed to continuous improvement, both in the code I write and the practices of the team
+around me, and to a culture of mentorship and knowledge sharing.
+`;
